8c58e3a46d406faf27bfe98f4364acb0aa286f3e
leo <cedillo47@gmail.com>
2021-09-28 19:30:47 +0000

    work 1-7

--- index.js
+++ index.js
@@ -1,1 +1,103 @@
 //Code your solutions in this file
+function fiveToOneHundred(){
+    for ( var i = 5; i <= 100; i++){
+        console.log(i)
+    }
+};
+
+function multiplesOfThreeOrFive(){
+    for(var i = 0; i <= 100; i++){
+    if(i % 3 == 0 || i % 5 == 0){
+    console.log(i)
+    }   
+    }
+}
+
+function multiplesOfThree(){
+    for(var i = 0; i <= 100; i++)
+    if(i % 3 == 0){
+    console.log(i)
+    }
+}
+// console.log(fiveToOneHundred());
+
+//console.log(multiplesOfThree());
+
+function untilNum(num){
+    for (var i = 1; i<= num; i++){
+        console.log(i)
+    }
+    
+};
+
+function mulitply(num1,num2){
+    const mul = num1 * num2
+        return mul
+    }
+
+// console.log(mulitply(2,3))
+
+function isNegative(num){
+   if (num  < 0) {
+      return true
+   } else {
+      return false }
+}
+
+console.log(isNegative(1))
+
+//Code your solutions in this file
+function fiveToOneHundred(){
+    for ( var i = 5; i <= 100; i++){
+        console.log(i)
+    }
+};
+
+function multiplesOfThreeOrFive(){
+    for(var i = 0; i <= 100; i++){
+    if(i % 3 == 0 || i % 5 == 0){
+    console.log(i)
+    }   
+    }
+}
+
+function multiplesOfThree(){
+    for(var i = 0; i <= 100; i++)
+    if(i % 3 == 0){
+    console.log(i)
+    }
+}
+// console.log(fiveToOneHundred());
+
+//console.log(multiplesOfThree());
+
+function untilNum(num){
+    for (var i = 1; i<= num; i++){
+        console.log(i)
+    }
+    
+};
+
+function mulitply(num1,num2){
+    const mul = num1 * num2
+        return mul
+    }
+
+// console.log(mulitply(2,3))
+
+function isNegative(num){
+   if (num  < 0) {
+      return true
+   } else {
+      return false }
+}
+
+
+
+function triangleArea(b,h){
+    const ar = b * h;
+    const area = ar / 2;
+    return area
+    
+};
+console.log(triangleArea(10,60))
