c1490c7aa433123ef549a4d2e614a55e69d500d9
Crislp12 <cristopher.picopinos@gmail.com>
2022-03-23 18:08:25 +0000

    first Function

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 //Code your solutions in this file
+
+function fiveToOneHundred(){
+    
+    console.log("--q1---")
+    for (let i =5; i <= 100; i++){ // i will increase tpo 100
+        console.log(i); // this will log the results 
+    }
+    
+};
+ fiveToOneHundred()
+
+
