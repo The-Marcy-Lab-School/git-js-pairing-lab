6b4802da753d8685f28df75e085de97a30d33d82
farouk0197 <fkatakpaou@gmail.com>
2022-03-23 18:12:26 +0000

    second function

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ function countToHundred(){
   for (let i=5;i<=100; i++){console.log(i)}
 }
 countToHundred()
+
+
+function multiplesOf3(){
+    for (let i = 0; i < 100; i++){
+        if (i % 3 === 0 ){
+            console.log(i)
+        }
+    }
+}
+
+multiplesOf3()
