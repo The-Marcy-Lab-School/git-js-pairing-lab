2952e9b6737b5a4b5acd5a0c98a447083f4b544b
Domosayshello <dominique.arno@gmail.com>
2023-03-29 18:25:01 +0000

    problem 2 complete

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ function fiveToOneHundred() {
         console.log(i);
     } 
 }
+
+function multiplesOfThree(){
+    for(let i = 3; i >= 100; i+=3){
+        console.log(i)
+    }
+}
+multiplesOfThree()
