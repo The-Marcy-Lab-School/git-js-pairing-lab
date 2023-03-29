2a006249e54dc4a4cb36dd236e4c7518c7a44992
anyrbas11 <sabrynapd04@gmail.com>
2023-03-29 19:03:47 +0000

    six functions complete

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const fiveToOneHundred = () =>{
+//Question #2
+ fiveToOneHundred = () =>{
     for (let i=5; i<=500; i++){
         console.log(i);
     }
@@ -10,5 +11,33 @@ const multipleOfThree = () =>{
         console.log(i);
     }
 }
+//Question #3
+const multipleOfThreeOrFive = () =>{ 
+    for(let i=0; i<=100; i++){ 
+if (i % 5 ==0 || i%3==0){
+    console.log(i)
+}
+        }
+}
+//Question #4
+const untilNum = (num)=>{
+for(let i=1; i<=num; i++){
+        console.log(i);
+    }
 
-console.log(multipleOfThree());
+}
+//Question #5
+const multiply =(num, num2)=>{
+    return num*num2
+}
+const add =(num, num2)=>{
+    if (num==num2){
+    return (num+num2)*3
+    }
+    else{
+        return num+num2
+    }
+}
+
+
+console.log(add(4,4))
