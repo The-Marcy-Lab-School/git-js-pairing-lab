258bc355413aa9568975a9f74061b53b31938581
Berlineda <bfaurelus7@gmail.com>
2023-10-04 17:09:18 -0400

    solved problem 3

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 //Code your solutions in this file
-const fiveToOneHundred = () => {
+
+/*const fiveToOneHundred = () => {
 for (i =5; i <= 100; i++) {
     console.log(i)
 }
 }
 (fiveToOneHundred());
-
-
+*/
+/*
 //Problem 2
 const multiplesOfThree = () =>{
     for (let i = 0; i <= 100; i+=3)
@@ -15,3 +16,12 @@ const multiplesOfThree = () =>{
     }
 }
 multiplesOfThree();
+*/
+const multipleOfThreeOrFive = (num) => {
+    for (let i =0; i <= 100; i++  )
+    
+    if(i % 3 === 0 || i % 5 === 0 ) {
+        console.log(i);
+    }
+}
+multipleOfThreeOrFive(100)
