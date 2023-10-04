2b404d75839fe35d1d38c4fde8f9797fd7d846c4
Allancool9 <TheOfficialAllanRamirez@gmail.com>
2023-10-04 15:06:53 -0400

    Function 1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-//Code your solutions in this file
+//1
+const fiveToOneHundred = () => {
+    for (let i = 5; i <= 100; i++) {
+      console.log(i);
+    }
+  };
+  
+  
+  fiveToOneHundred();
+
+  
+
+  
+  
+  
