43f730b96b1e8c91b8ca86a65e4dc8d5eee090ca
Nathaniel W <52637288+nw-github@users.noreply.github.com>
2023-10-04 14:46:15 -0400

    completed question 3

--- index.js
+++ index.js
@@ -10,7 +10,21 @@ const fiveToOneHundred = () => {
 const multiplesOfThree = () => {
     for (let i = 1; i <= 100; i++) {
         if (i % 3 === 0) {
-            console.log(i)
+            console.log(i);
         }
     }
 };
+
+/* 
+Write a function multiplesOfThreeOrFive that console logs all positive numbers to 100 if that number is either a mulitple of 3 or a mutliple of 5.
+*/
+
+
+const multiplesOfThreeOrFive = () => {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            console.log(i);
+        }
+    }
+};
+multiplesOfThreeOrFive();
