0e4e76e703432c848f4670bdd112f36a052d6e89
MohamedSy1 <mohamed.sy@compscihigh.org>
2023-10-04 14:37:45 -0400

    completed question 2

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const fiveToOneHundred = () => {
         console.log(i);
     }
 };
+
+//Write a function multiplesOfThree that console logs all positive numbers to 100 that is a multiple of 3.
+
+
+const multiplesOfThree = () => {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0) {
+            console.log(i)
+        }
+    }
+};
