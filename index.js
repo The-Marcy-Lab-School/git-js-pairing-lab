af393169023c36225ce4ee6c2f3b1363b32f506f
mayayr115 <mayaramkishuncsh2027@gmail.com>
2023-10-04 21:50:12 -0400

    Override Error

--- index.js
+++ index.js
@@ -6,13 +6,10 @@ fiveToOneHundred();
 const multiplesOfThree = () => {for (i = 0; i < 100; i++){if (i % 3 === 0) {console.log(i)}}}
 multiplesOfThree()
 
-
 //3
-const multiplesOfThreeOrFive = () => {
-  for (let i = 3; i <= 100; i += 3) {
-    console.log(i);
-  
-  }
-  
-};
+const multiplesOfThreeOrFive = () => {for (let i = 3; i <= 100; i += 3) {console.log(i);}};
 multiplesOfThreeOrFive()
+
+//4
+const untilNum = num => {for (i = 1; i <= num; i++){console.log(i)}}
+untilNum(9)
