7cd2bd6b068b1b172a461973e129a35f936f1bf1
Nathaniel W. <52637288+nw-github@users.noreply.github.com>
2023-10-04 22:13:41 -0400

    wrote a (terrible) test runner and some tests

--- index.js
+++ index.js
@@ -42,12 +42,6 @@ const add = (a, b) => {
     return a === b ? (a + b) * 3 : a + b;
 };
 
-// console.log(add(2, 4));
-// console.log(add(10, -5));
-// console.log(add(3, 7.5));
-// console.log(add(5, 5));
-// console.log(add(6, 6));
-
 const isNegative = (num) => {
     if(Math.sign(num) === -1 ){
     console.log(true)
@@ -55,10 +49,6 @@ const isNegative = (num) => {
     console.log(false)
    }
 }
-
-// isNegative(3)         //returns false
-// isNegative(-1)        //returns true
-// isNegative(Math.PI)   //returns false
 
 // 8
 const triangleArea = (height, base) => 0.5 * base * height;
@@ -81,5 +71,74 @@ const changeString = (str) => {
     return String.fromCharCode(...[...str].map(ch => ch.charCodeAt(0) + 1));
 };
 
-console.log(changeString("abc"))             //return "bcd"
-console.log(changeString("helloworld"))      //return "ifmmpxpsme"
+
+
+
+
+
+(() => {
+    const test = (name, fn) => {
+        const header = `${name}...`.padEnd(30);
+        try {
+            fn();
+            console.log(header, "passed!");
+        } catch (ex) {
+            console.log(header, `${ex}`);
+        }
+    };
+
+    const assertEq = (a, b) => {
+        if (a === b) return;
+        throw new Error(`expected ${b}, got ${a}`);
+    };
+
+    test("multiply", () => assertEq(multiply(2, 4), 8));
+    test("multiply", () => assertEq(multiply(10, -5), -50));
+    test("multiply", () => assertEq(multiply(3, 7.5), 22.5));
+
+    test("add", () => assertEq(add(2, 4), 6));
+    test("add", () => assertEq(add(10, -5), 5));
+    test("add", () => assertEq(add(3, 7.5), 10.5));
+    test("add", () => assertEq(add(5, 5), 30));
+    test("add", () => assertEq(add(6, 6), 36));
+
+    test("isNegative", () => assertEq(isNegative(3), false));
+    test("isNegative", () => assertEq(isNegative(-2), true));
+    test("isNegative", () => assertEq(isNegative(Math.PI), false));
+
+    test("triangleArea", () => assertEq(triangleArea(5, 7), 17.5));
+    test("triangleArea", () => assertEq(triangleArea(6, 8), 24));
+
+    test("betweenTwentyAndFourty", () => assertEq(betweenTwentyAndFourty(4), false));
+    test("betweenTwentyAndFourty", () => assertEq(betweenTwentyAndFourty(21), true));
+    // XXX: the readme says this should return true, but obviously
+    // 99 is not between 20 and 40. assuming that is a error.
+    test("betweenTwentyAndFourty", () => assertEq(betweenTwentyAndFourty(99), false /* true */));
+    test("betweenTwentyAndFourty", () => assertEq(betweenTwentyAndFourty(101), false));
+
+    test("largest", () => assertEq(largest(4, 6, 8), 8));
+    test("largest", () => assertEq(largest(30, 22, 17), 30));
+    test("largest", () => assertEq(largest(41, 108, 86), 108));
+
+    // TODO: printTime tests
+
+    test("isLeapYear", () => assertEq(isLeapYear(2000), true));
+    test("isLeapYear", () => assertEq(isLeapYear(1900), false));
+    test("isLeapYear", () => assertEq(isLeapYear(2020), true));
+    test("isLeapYear", () => assertEq(isLeapYear(1999), false));
+
+    // NOTE: getExtention is a typo
+    test("getExtension", () => assertEq(getExtension("hello.txt"), ".txt"));
+    test("getExtension", () => assertEq(getExtension("app.js"), ".js"));
+    test("getExtension", () => assertEq(getExtension("README.md"), ".md"));
+
+    // TODO: absoluteNineteen tests
+
+    test("switchLetters", () => assertEq(switchLetters("anne"), "enna"));
+    test("switchLetters", () => assertEq(switchLetters("hello world"), "dello worlh"));
+    test("switchLetters", () => assertEq(switchLetters("a"), "a"));
+    test("switchLetters", () => assertEq(switchLetters(""), ""));
+
+    test("changeString", () => assertEq(changeString("abc"), "bcd"));
+    test("changeString", () => assertEq(changeString("helloworld"), "ifmmpxpsme"));
+})();
