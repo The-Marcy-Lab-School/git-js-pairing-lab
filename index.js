260327a1b93e5540d912977cb76d4d1597308ce6
toeten <n.santiago0228@gmail.com>
2024-03-27 16:59:42 -0400

    answered question 2

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ const fiveToOneHundred = () => {
     }
 }
 fiveToOneHundred()
+
+const multiplesOfThree = () => {
+    for (let i = 0; i < 101; i++) {
+        console.log(i * 3)
+    }
+}
+
+multiplesOfThree()
