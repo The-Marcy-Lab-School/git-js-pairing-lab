e331f470c8855a2d800064eccd52dec5bd787fce
Abdullahk1105 <Abdullahk11051@gmail.com>
2024-03-28 15:03:35 -0400

    Problem 2 solved

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 //Code your solutions in this file
+const multiplesOfThree = () => {
+    for (let i = 3; i <= 100; i++) {
+        if (i % 3 == 0) {
+            console.log(i);
+        }
+    }
+}
+console.log(multiplesOfThree(100))
