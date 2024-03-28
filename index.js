f61fd4a86f36a8c94aec15bd83ec1204a090873c
Taifinkaba <taifinkabad@gmail.com>
2024-03-28 15:46:39 -0400

    question7Done

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const multiplesOfThreeOrFive =() => {
 multiplesOfThreeOrFive();
 
 
-// question 5 completed
+// question 4 completed
 const untilNum = (num) => {
     for (let i = 1; i <= num; i++) {
       console.log(i);
@@ -39,17 +39,26 @@ const untilNum = (num) => {
 
 untilNum(5);
 
-
+// question 5
 const multiply = (num1, num2) => {
     return num1 * num2
 };
 
 console.log(multiply(3, 5));
-*/
 
+// queestion 6
 const add = (num1, num2) => {
     return num1 === num2 ? 3 * (num1 + num2) : num1 + num2;
   };
 
 
   console.log(add(3, 5));
+  */
+
+  // question 7
+  const isNegative = (num1) =>{
+    return num1 < 0;
+  }
+  console.log(isNegative(-1));
+
+
