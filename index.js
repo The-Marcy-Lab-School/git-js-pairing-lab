b3d81cf96c7b39e2e8eeea6adafe1b0a6df7e8a3
Taifinkaba <taifinkabad@gmail.com>
2024-03-28 14:18:46 -0400

    question_3_completed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 /*
+// queestion one tested and complete
 //Code your solutions in this file
 const fiveToOneHundred = () => {
     for(let i = 5; i<=100; i++){
@@ -7,14 +8,24 @@ const fiveToOneHundred = () => {
 }
 
 fiveToOneHundred();
-// queestion one tested and complete
-*/
 
 
+// queestion two tested and complete
 const multiplesOfThree = () => {
     for (let i = 3; i <= 100; i += 3) {
       console.log(i);
     }
   };
   multiplesOfThree();
+  */
+
+ // queestion thre tested and complete 
+const multiplesOfThreeOrFive =() => {
+    for(let i=3; i <= 100; i++){
+        if(i % 3 === 0 || i % 5 === 0) {
+            console.log(i)
+        }
+    }
+}
+multiplesOfThreeOrFive();
   
