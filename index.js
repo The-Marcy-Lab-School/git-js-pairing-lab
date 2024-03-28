d56458809a76e98a5b1410581a5862aedfe14abd
OcAdriann <ocadriannnc20@gmail.com>
2024-03-28 13:24:22 -0400

    Question 1 done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 //Code your solutions in this file
+const fiveToOneHundred = () => {
+    let i = 5
+    for (i; i < 101; i++) {
+        console.log(i);
+    }
+    return;
+}
+
+fiveToOneHundred()
