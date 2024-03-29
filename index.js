9d0d772c6785e91e72dc443933e2ed39fe3cee9f
Patrick Dacius <Patrickdacius27@gmail.comgit config --global credential.helper store>
2024-03-29 17:57:04 -0400

    problem 2 solved

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-//Code your solutions in this file
+// Patrick-problem 2: mulitplesOfThree()
+
+const multiplesOfThree = () => {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 == 0) {
+            console.log(i)
+        }
+    }
+}
+
+console.log(multiplesOfThree())
