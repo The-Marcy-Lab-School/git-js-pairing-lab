4f54c3ad48122041680e5fc757d2991a36dec041
kurasmagnolia <kingmmcleod@outlook.com>
2024-10-02 13:57:34 -0400

    Function 1

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 //Code your solutions in this file
+
+// Problem 1
+const fiveToOneHundred = () => {
+    for (let i = 5; i <= 100; i++) {
+        console.log(i);
+    }
+};
+
+console.log('Problem 1:');
+fiveToOneHundred();
